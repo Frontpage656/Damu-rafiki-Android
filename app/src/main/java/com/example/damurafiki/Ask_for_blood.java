0d5c2640f689af7cb916c1f441c0ddf27db7e1e5
frontpage community <joshuasimon656@gmail.com>
2022-06-05 21:21:06 -0700

    Damu rafiki ongoing

--- app/src/main/java/com/example/damurafiki/Ask_for_blood.java
+++ app/src/main/java/com/example/damurafiki/Ask_for_blood.java
@@ -1,8 +1,8 @@
 package com.example.damurafiki;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -10,14 +10,23 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Ask_for_blood extends AppCompatActivity {
 
     EditText id_number;
     ImageView scan_qr;
-
     ProgressDialog dialog;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,20 @@ public class Ask_for_blood extends AppCompatActivity {
         scan_qr = findViewById(R.id.scan_qr);
 
         dialog = new ProgressDialog(Ask_for_blood.this);
+
+
+        scan_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(Ask_for_blood.this);
+                intentIntegrator.setBeepEnabled(true)
+                        .setPrompt("kuongeza mwanga tumia +v")
+                        .setOrientationLocked(true)
+                        .setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+            }
+        });
+
         id_number.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -48,22 +71,40 @@ public class Ask_for_blood extends AppCompatActivity {
 
                     int count = id_number.length();
                     if (count == 9) {
+                        dialog.setMessage("Subiri kidogo...");
+                        dialog.show();
                         id_number.setEnabled(false);
-                        if (id_number.getText().toString().trim().equals("123456789")) {
-                            dialog.show();
-                            dialog.setMessage("Alomost there...");
+
+                        db.collection("find_donor").document(id_number.getText().toString())
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.getResult().exists()) {
+                                            dialog.dismiss();
+
+                                            Intent intent = new Intent(getApplicationContext(),Post_it.class);
+                                            intent.putExtra("id",id_number.getText().toString());
+                                            startActivity(intent);
+                                            finish();
 
 
+                                        } else {
+                                            dialog.dismiss();
+                                            Toast.makeText(Ask_for_blood.this, "Nothing like this", Toast.LENGTH_SHORT).show();
+                                            id_number.setText("");
+                                            id_number.setEnabled(true);
+                                        }
+                                    }
+                                });
 
-                        } else {
-                            Toast.makeText(Ask_for_blood.this, "Wrong id!!!!", Toast.LENGTH_SHORT).show();
-                            id_number.setEnabled(true);
-                            id_number.setText("");
-                        }
+                    } else {
+                        id_number.setEnabled(true);
                     }
 
+
                 }
-    }
+            }
 
             @Override
             public void afterTextChanged(Editable editable) {
@@ -74,5 +115,27 @@ public class Ask_for_blood extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result.getContents() != null) {
+            if (result.getContents().trim().length() == 9) {
+
+                id_number.setText(result.getContents());
+
+            } else {
+
+                Toast.makeText(this, "Namba sio sahihi", Toast.LENGTH_SHORT).show();
+                id_number.setText("");
+                id_number.setEnabled(true);
+            }
+        } else {
+            Toast.makeText(this, "Rudia kuscann qr code", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/damurafiki/Post_it.java
@@ -0,0 +1,96 @@
+package com.example.damurafiki;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Post_it extends AppCompatActivity {
+
+    TextInputEditText explanation;
+    TextView count, post, blood_group, person_name, chupa_count, time;
+
+    String the_coming_id;
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_it);
+
+        Intent come_intent = getIntent();
+
+
+        the_coming_id = come_intent.getStringExtra("id");
+
+        explanation = findViewById(R.id.explanation);
+        count = findViewById(R.id.count);
+        post = findViewById(R.id.post);
+
+        blood_group=findViewById(R.id.blood_group);
+        person_name=findViewById(R.id.person_name);
+        chupa_count=findViewById(R.id.chupa_count);
+        time=findViewById(R.id.time);
+
+
+        firestore.collection("find_donor").document(the_coming_id).get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    blood_group.setText(String.valueOf(task.getResult().get("blood_group")));
+                                    chupa_count.setText(String.valueOf(task.getResult().get("chupa_count")));
+                                    person_name.setText(String.valueOf(task.getResult().get("mgojwa_name")));
+                                    time.setText(String.valueOf(task.getResult().get("time")));
+                                    
+                            }
+                        });
+
+
+                        explanation.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                            }
+
+                            @SuppressLint("ResourceAsColor")
+                            @Override
+                            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                                int count_value = charSequence.length();
+                                int remain_value = 100 - count_value;
+
+                                if (remain_value > 1) {
+                                    count.setText(String.valueOf(remain_value));
+                                } else {
+                                    count.setText(String.valueOf(remain_value));
+                                    count.setTextColor(R.color.my_primary_color);
+                                    Toast.makeText(Post_it.this, "Imezidi idadi", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                            @Override
+                            public void afterTextChanged(Editable editable) {
+
+                            }
+                        });
+
+
+                    }
+                }
+
+
+
+
+
